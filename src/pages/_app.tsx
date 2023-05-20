a1ae62c804fa312ddb7016313065492987d28d0e
lawrence nicastro <lawrence.nicastro1@gmail.com>
2023-05-20 16:01:49 -0400

    checkpoint: fix middleware location as it was causing issues with clerkdev

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider>
+    <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
